686e9a69ff44bc019ef752bd7c404959889ae106
Mishbah <mishbah@jp74.com>
2014-09-08 23:18:10 +0100

    Add more tests for custom context_processor

--- tests/test_context_processors.py
+++ tests/test_context_processors.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
 from django.test import TestCase
 
 from django.contrib.sites.models import Site
@@ -31,6 +32,20 @@ class MiddlewareTest(TestCase):
 
     def test_context_processors(self):
         """ Test the `usersettings` attribute is available as context in templates """
+        resp = self.client.get(reverse('current_usersettings'))
+        self.assertEqual(200, resp.status_code)
+        self.failUnless(isinstance(resp.context['usersettings'], self.usersettings_model))
+
+    @override_settings(
+        MIDDLEWARE_CLASSES=(
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',)
+    )
+    def test_context_processors_without_middleware(self):
+        """
+            Test usersettings context_processors still works without the custom middleware
+        """
         resp = self.client.get(reverse('current_usersettings'))
         self.assertEqual(200, resp.status_code)
         self.failUnless(isinstance(resp.context['usersettings'], self.usersettings_model))
