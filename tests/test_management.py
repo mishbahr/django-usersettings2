7fd7d9a8cc5917179e07b7fd1daef9cb7d3ba9d9
Andrey Kolpakov <aakolpakov@gmail.com>
2014-09-25 22:40:34 +0200

    Check integrity exception in "usersettings_init"

--- tests/test_management.py
+++ tests/test_management.py
@@ -6,6 +6,7 @@ from usersettings.shortcuts import get_usersettings_model
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.contrib.sites.models import Site
+from django.core.management.base import CommandError
 
 
 class CommandsTest(TestCase):
@@ -13,17 +14,50 @@ class CommandsTest(TestCase):
     email = 'superuser@example.com'
     password = 'pa$sw0Rd'
 
-    usersettings_model = get_usersettings_model()
-
     def setUp(self):
         Site.objects.get_or_create(id=settings.SITE_ID, domain='example.com', name='example.com')
+
+    def setUpUser(self):
         self.user = get_user_model().objects.create_superuser(self.username, self.email, self.password)
 
     def test_usersettings_init(self):
+        self.setUpUser()
+
         call_command('usersettings_init')
-        self.assertEqual(self.usersettings_model.objects.all().count(), 1)
+
+        self.assertEqual(get_usersettings_model().objects.all().count(), 1)
+
+    def test_usersettings_init_many(self):
+        self.setUpUser()
+
+        Site.objects.get_or_create(id=(settings.SITE_ID + 1), domain='example.com', name='example.com')
+
+        call_command('usersettings_init')
+
+        self.assertEqual(get_usersettings_model().objects.all().count(), 2)
 
     def test_usersettings_init_allready_exists(self):
+        self.setUpUser()
+
         call_command('usersettings_init')
         call_command('usersettings_init')
-        self.assertEqual(self.usersettings_model.objects.all().count(), 1)
+
+        self.assertEqual(get_usersettings_model().objects.all().count(), 1)
+
+    def test_usersettings_no_user(self):
+        with self.assertRaises(CommandError):
+            call_command('usersettings_init')
+
+        self.assertEqual(get_usersettings_model().objects.all().count(), 0)
+
+    def test_usersettings_integrity_constraint(self):
+        self.setUpUser()
+
+        # little hack, but don't know another way to test
+        get_usersettings_model()._meta.get_field_by_name('tag_line')[0].empty_strings_allowed = False
+
+        with self.assertRaises(CommandError):
+            call_command('usersettings_init')
+
+        self.assertEqual(get_usersettings_model().objects.all().count(), 0)
+
--- usersettings/management/commands/usersettings_init.py
+++ usersettings/management/commands/usersettings_init.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand, CommandError
 from usersettings.shortcuts import get_usersettings_model
 from django.contrib.sites.models import Site
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
@@ -22,12 +23,16 @@ class Command(BaseCommand):
         for site in Site.objects.all():
             print('Processing site %s' % site)
 
-            config, created = get_usersettings_model().objects.get_or_create(
-                site=site,
-                user=user
-            )
+            try:
+                config, created = get_usersettings_model().objects.get_or_create(
+                    site=site,
+                    user=user
+                )
 
-            if not created:
-                print('Userconfig for site %s already exists' % Site.objects.get_current())
+                if not created:
+                    print('Userconfig for site %s already exists' % Site.objects.get_current())
+
+            except IntegrityError as e:
+                raise CommandError("Couldn't create userconfig record. Please check default values. Error %s" % e)
 
         print('Done')
